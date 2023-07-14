dac899af7a1e48ceb722cfb9653093c61b84281a
marino-png <marino@calcagni.eu>
2023-07-14 19:14:11 +0100

    This commit marks the completion of the grid project's core functionality. Users can interact with the grid by pressing the button, but it can only be pressed once due to current limitations. However, there is still work to be done to improve the aesthetics of the grid. The colors assigned to the cells are currently incorrect, requiring further adjustments to ensure a visually pleasing experience. Future updates will address these issues, refining the user interface to create a more visually appealing grid.

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const container = document.querySelector('.container');
+const gridElements = [];
+
 for (let i = 0; i < 256; i++){
     const div = document.createElement('div');
     div.classList.add('element');
+    div.style.flexBasis = '6.25%';
     container.appendChild(div);
+    gridElements.push(div);
+
     div.onmouseover = function() {
         div.classList.add('mouseOver');
     };
+
     div.onmouseout = function() {
         div.classList.remove('mouseOver');
-    };
+    };   
 }
 
+function remove(){
+    for (let i = 0; i < 256; i++){
+        container.removeChild(gridElements[i]);
+    }
+}
+
+function chooseGrid(){
+    remove();
+    let number = prompt('please enter a number' );
+    let numericValue = parseFloat(number);
+    let percentage = (100/numericValue)+'%';
+    number = Math.pow(number,2);
+
+    for (let i = 0; i < number; i++){
+        const div = document.createElement('div');
+        div.classList.add('element');
+        container.appendChild(div);
+        div.style.flexBasis = percentage;
+        gridElements.push(div);
+    
+        div.onmouseover = function() {
+            div.classList.add('mouseOver');
+        };
+    
+        div.onmouseout = function() {
+            div.classList.remove('mouseOver');
+        };   
+    }
+}
